1a458f0290016a9adfe8e59bcde68e92ba1d95d8
Bassamhussam <62281597+Bassamhussam@users.noreply.github.com>
2025-04-19 19:13:48 +0200

    Update GPIOTest.c

--- Test/GPIOTest.c
+++ Test/GPIOTest.c
@@ -9,3 +9,14 @@
 #include "..\HAL\PORTF\PORTF.c"
 
 void SystemInit() {};
+
+void delay_ms(uint32 ms)
+{
+    volatile uint32 count;
+    while (ms--)
+    {
+        for (count = 0; count < 4000; count++)
+        {
+        } // adjust for your clock speed
+    }
+}
