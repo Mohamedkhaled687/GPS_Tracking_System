c3aa0ec24dc856ff08ecbff900b7dcf824901ae0
Bassamhussam <62281597+Bassamhussam@users.noreply.github.com>
2025-04-19 18:30:18 +0200

    Update PORTF.h

--- HAL/PORTF/PORTF.h
+++ HAL/PORTF/PORTF.h
@@ -1,1 +1,16 @@
+/******************************************************************************
+ *
+ * Module: PORTF
+ *
+ * File Name : PORTF.h
+ *
+ * Description: Header File for Tiva C PortF Driver
+ *
+ * Author: Bassam Hussam
+ *
+ *******************************************************************************/
 
+#ifndef PORTF_HEADER
+#define PORTF_HEADER
+
+#endif 
