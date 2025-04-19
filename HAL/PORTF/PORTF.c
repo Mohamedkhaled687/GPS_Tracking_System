c416bfb5139d7b2a3d7323ef053f303658ad73af
Bassamhussam <62281597+Bassamhussam@users.noreply.github.com>
2025-04-19 19:05:06 +0200

    Update PORTF.c

--- HAL/PORTF/PORTF.c
+++ HAL/PORTF/PORTF.c
@@ -28,3 +28,20 @@ void PORTF_LEDS_Init(void)
     GPIO_PORTF_DEN_R |= LEDS_MASK;
     GPIO_PORTF_DATA_R &= ~LEDS_MASK; // intialize pins 1,2,3 to be off
 }
+
+/*
+ * Description :
+ * Initialize the Switches of PORTF.
+ */
+void PORTF_SW1_SW2_Init(void)
+{
+    GPIO_Init(GPIO_PORTF);
+
+    GPIO_PORTF_CR_R |= SW_MASK;       // setting pin p4 to change
+    GPIO_PORTF_AMSEL_R &= ~SW_MASK;   // disable analog function
+    GPIO_PORTF_PCTL_R &= ~0x000F000F; // GPIO CLEAR BIT PCTL
+    GPIO_PORTF_AFSEL_R &= ~SW_MASK;   // disable alterante function
+    GPIO_PORTF_DIR_R &= ~SW_MASK;     // DIR--> 0->input & 1->output
+    GPIO_PORTF_PUR_R |= SW_MASK;      // active low
+    GPIO_PORTF_DEN_R |= SW_MASK;      // Enable digital for sw1
+}
