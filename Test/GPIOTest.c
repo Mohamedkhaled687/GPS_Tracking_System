2b05151910a822083e917a4ac56b57074d06cf1e
Bassamhussam <62281597+Bassamhussam@users.noreply.github.com>
2025-04-19 19:14:56 +0200

    Update GPIOTest.c

--- Test/GPIOTest.c
+++ Test/GPIOTest.c
@@ -25,4 +25,20 @@ int main(void)
 {
     PORTF_LEDS_Init();     // Initialize the PORTF LEDs
     PORTF_SWITCHES_Init(); // Initialize the PORTF Switches
+
+    
+    while (1)
+    {
+
+        if (PORTF_ReadSwitch(SW1) == SW_PRESSED) // Check if SW2 is pressed
+        {
+            PORTF_SetLed(GREEN_LED, LED_ON); // Turn on the green LED
+        }
+        else
+        {
+            PORTF_SetLed(GREEN_LED, LED_OFF); // Turn off the green LED
+        }
+
+        return 0; // Return success
+    }
 }
