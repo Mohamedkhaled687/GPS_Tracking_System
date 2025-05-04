afd16c22c7e999a1f25d4177d0dd606a9272bb49
ahmedstudies <ahmedthanawy15@gmail.com>
2025-05-05 02:45:59 +0300

    GPS Driver Completed

--- HAL/GPS/GPS.h
+++ HAL/GPS/GPS.h
@@ -1,12 +1,9 @@
-
-
 #define PI 3.14159265359
 
-const double EARTH_RADIUS = 6378000;
+const double EARTH_RADIUS = 6371000; //earth_radius in meters
 
 /* PROTOTYPES */
 void Get_GPRMC();
 void parse_GPRMC();
-float convertToDecimalDegrees(float nmeaCoord);
 float convertToRad(float degrees);
-float Calculate_Distance(float lat1, float long1, float lat2, float long2);
+float Calculate_Distance(float lat2, float long2);
---
+++ Test/GPSTest.c
@@ -0,0 +1,65 @@
+/******************************************************************************
+ *
+ * Module: LED
+ *
+ * File Name : LED.h
+ *
+ * Description: Header File for Tiva C LED Driver
+ *
+ * Author: Mohamed Khaled
+ *
+ *******************************************************************************/
+
+ #include "stdio.h"
+ #include "..\MCAL\UART\UART.c"
+ #include "..\HAL\PORTF\PORTF.c"
+ #include "..\HAL\GPS\GPS.c"
+ //30.064079790586238, 31.279363300577383 | 30.064139590172832, 31.279339980915793
+ void SystemInit() {};
+ float lat2 = 30.063768832165547, long2 = 31.279524810824068; //Choose What you want
+ //char GPS_output[80] = "$GPRMC,123519.00,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A";
+ /*
+ void delay_ms(uint32 ms)
+ {
+     volatile uint32 count;
+     while (ms--)
+     {
+         for (count = 0; count < 4000; count++)
+         {
+         } // adjust for your clock speed
+     }
+ }*/
+
+ int main(void)
+ {
+    float distance;
+		char i;
+    UART_ConfigType UART_Configurations; // UART configuration structure
+
+    // Configure UART settings
+    UART_Configurations.uart_number = UART0; // Set UART number
+    UART_Configurations.DataBits = 8;        // 8 data bits
+    UART_Configurations.parity = 0;          // No parity
+    UART_Configurations.stop_bits = 1;       // 1 stop bit
+    UART_Configurations.IBRD = 104;          // Integer Baud Rate (for 9600 baud with 16 MHz clock)
+    UART_Configurations.FBRD = 11;           // Fractional Baud Rate
+		UART_Config(&UART_Configurations); // Initialize UART with settings
+	 
+		PORTF_SW1_SW2_Init();
+		PORTF_LEDS_Init();
+	 
+     while (1)
+     {
+        Get_GPRMC();
+        parse_GPRMC();
+        distance = Calculate_Distance(lat2, long2);
+        if (distance > 37) {
+                PORTF_led_Toggle(RED);
+        }
+        else {
+                PORTF_led_Toggle(GREEN);
+        }
+     }
+ 
+     return 0; // Return success
+ }
