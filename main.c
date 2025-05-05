f79aaed48ad15e3172fcb0e99899f72ae7cea4e3
ahmedstudies <ahmedthanawy15@gmail.com>
2025-05-05 22:17:54 +0300

    Added testcode in main.c for both TFT and SSI

--- main.c
+++ main.c
@@ -1,35 +1,46 @@
 /******************************************************************************
  *
- * Module: LED
+ * Module: TFT and SSI
  *
- * File Name : LED.h
+ * File Name : TFT.h
  *
- * Description: Header File for Tiva C LED Driver
+ * Description: Header File for Tiva C TFT and SSI
  *
- * Author: Mohamed Khaled
+ * Author: Ahmed Mustafa
  *
  *******************************************************************************/
 
-#include "stdio.h"
-#include "HAL\PORTF\PORTF.c"
-#include "MCAL\SysticTimer\SYSTICK.h"
-
-void SystemInit() {};
-
-int main(void)
-{
-
-    PORTF_SW1_SW2_Init();
-    PORTF_LEDS_Init();
-    SysTick_Init();
-
-    while (1)
-    {
-        PORTF_SetLedValue(BLUE, LED_ON);
-        SysTick_DelayMs(1000);
-        PORTF_SetLedValue(BLUE, LED_OFF);
-        SysTick_DelayMs(1000);
-    }
-
-    return 0; // Return success
-}
+ #include "HAL\TFT\TFT.h"
+ #include "HAL\TFT\TFT.c"
+ #include "MCAL/SSI/SSI.h"
+ 
+ //30.064079790586238, 31.279363300577383 | 30.064139590172832, 31.279339980915793
+ void SystemInit() {};
+ //char GPS_output[80] = "$GPRMC,123519.00,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A";
+ void delay_ms(uint32 ms)
+ {
+     volatile uint32 count;
+     while (ms--)
+     {
+         for (count = 0; count < 4000; count++)
+         {
+         } // adjust for your clock speed
+     }
+ }
+ 
+ int main(void)
+ {
+ uint8 i;
+ TFT_init();
+ 
+ while(1) {
+     
+     for (i = 0; i < 50; i++) {
+         SSI_Send(SSI0, &i);
+         delay_ms(10);
+     }
+     
+ }
+ 
+     return 0; // Return success
+ }
