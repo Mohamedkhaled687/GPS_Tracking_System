1c96fbfa32504b969067f0617f82259f84c930b7
ahmedstudies <ahmedthanawy15@gmail.com>
2025-05-07 13:36:37 +0300

    update

--- HAL/GPS/GPS.c
+++ HAL/GPS/GPS.c
@@ -1,6 +1,4 @@
 #include "GPS.h"
-#include "../../LIB/common_macros.h"
-#include "../../LIB/tm4c123gh6pm.h"
 #include "stdlib.h"
 #include <math.h>
 #include "../../MCAL/UART/UART.h"
@@ -11,100 +9,104 @@ char GPS_logName[] = "$GPRMC,";
 char GPS_output[80];
 char GPS_formated[12][20];
 char *token;
-//30.063768832165547, 31.279524810824068 (MIDDLE OF 11YARD PLAYGROUND IN COLLEGE)
+// 30.063768832165547, 31.279524810824068 (MIDDLE OF 11YARD PLAYGROUND IN COLLEGE)
 float lat1, long1;
-float lat2 = 30.063768832165547, long2 = 31.279524810824068; //Choose What you want
+float lat1_temp, long1_temp, lat2_temp, long2_temp;
 
-void Get_GPRMC() {
-    //Recieve Correct Log
-		char i;
+void Get_GPRMC()
+{
+    // Recieve Correct Log
+    char i;
     char flag = 1;
-		char recievedByte;
+    char recievedByte;
     char fillCounter = 0;
-    UART_recieveString(UART0, GPS);
-    do {
+    // UART_recieveString(UART0, GPS);
+    do
+    {
         flag = 1;
-        for (i = 0; i < 7; i++) {
-            if (GPS_logName[i] != UART_recieveByte(0)) {
-                flag = 0; break;
+        for (i = 0; i < 7; i++)
+        {
+            if (GPS_logName[i] != UART_recieveByte(0))
+            {
+                flag = 0;
+                break;
             }
         }
     } while (flag == 0);
-    
+
     strcpy(GPS, "");
 
-    
-/*
-    **THERE ARE 2 ALTERNATIVES FOR WRITING THE CODE THAT WILL WRITE TO GPS ARRAY**
-    **IN THE VIDEO, THE ONE THAT IS COMMENTED BELOW ON LINE 32 IS THE ONE WRITTEN**
-    **I DO NOT KNOW WHY HE DIDN'T WRITE IT LIKE THE ONE I WROTE UNDER IT WHICH IS NOT COMMENTED**
-*/
-
-    /*
-    do {
+    do
+    {
         recievedByte = UART_recieveByte(0);
         GPS[fillCounter++] = recievedByte;
-    }while(recievedByte != '*');
-    */
-    do {
-        GPS[fillCounter++] = UART_recieveByte(0);
-    }while(recievedByte != '*');
-
+    } while (recievedByte != '*');
 }
 
-void parseGPRMC() {
+void parse_GPRMC()
+{
     char noOfTokenStrings = 0;
-    
+
     token = strtok(GPS, ",");
 
-    do {
+    do
+    {
         strcpy(GPS_formated[noOfTokenStrings], token);
         token = strtok(NULL, ",");
         noOfTokenStrings++;
 
-    }while(token != NULL);
+    } while (token != NULL);
 
-    if (noOfTokenStrings == 11) {
-        if (strcmp(GPS_formated[1], "A") == 0) {
-            if (strcmp(GPS_formated[3], "N") == 0) {
+    if (noOfTokenStrings == 11)
+    {
+        if (strcmp(GPS_formated[1], "A") == 0)
+        {
+            if (strcmp(GPS_formated[3], "N") == 0)
+            {
                 lat1 = atof(GPS_formated[2]);
-            } else {
+            }
+            else
+            {
                 lat1 = -atof(GPS_formated[2]);
             }
-            if (strcmp(GPS_formated[5], "E") == 0) {
+            if (strcmp(GPS_formated[5], "E") == 0)
+            {
                 long1 = atof(GPS_formated[4]);
-            } else {
+            }
+            else
+            {
                 long1 = atof(GPS_formated[4]);
             }
         }
     }
 }
 
-float convertToRad(float degrees) {
-    return degrees * (PI/180);
+float convertToRad(float degrees)
+{
+    return degrees * (PI / 180);
 }
-/*
-    lat1 = current
-    lat2 = destination
-*/
-float Calculate_Distance (float lat1, float long1, float lat2, float long2) {
-		float diff_lat;
-		float diff_long;
-		float a;
-		float c;
+/**
+ * @param lat2 Desination Lat
+ * @param long2 Destination Long
+ */
+float Calculate_Distance(float lat2, float long2)
+{
+    float diff_lat;
+    float diff_long;
+    float a;
+    float c;
     // Convert to Rad
-    lat1 = convertToRad(lat1);
-    long1 = convertToRad(long1);
-    lat2 = convertToRad(lat2);
-    long2 = convertToRad(long2);
+    lat1_temp = convertToRad(lat1);
+    long1_temp = convertToRad(long1);
+    lat2_temp = convertToRad(lat2);
+    long2_temp = convertToRad(long2);
     // Differences
-    diff_lat = lat2 - lat1;
-    diff_long = long2 - long1;
-    //Haversine Formula
-    a = sin(diff_lat/2) * sin(diff_lat/2) + cos(lat1) * cos(lat2) * sin(diff_long/2) * sin(diff_long/2);
+    diff_lat = lat2_temp - lat1_temp;
+    diff_long = long2_temp - long1_temp;
+    // Haversine Formula
+    a = sin(diff_lat / 2) * sin(diff_lat / 2) + cos(lat1_temp) * cos(lat2_temp) * sin(diff_long / 2) * sin(diff_long / 2);
 
-    c = 2* atan2(sqrt(a), sqrt(1-a));
+    c = 2 * atan2(sqrt(a), sqrt(1 - a));
 
     return EARTH_RADIUS * c;
-
 }
--- HAL/GPS/GPS.h
+++ HAL/GPS/GPS.h
@@ -1,5 +1,3 @@
-
-
 #define PI 3.14159265359
 
 const double EARTH_RADIUS = 6371000; //earth_radius in meters
@@ -8,4 +6,4 @@ const double EARTH_RADIUS = 6371000; //earth_radius in meters
 void Get_GPRMC();
 void parse_GPRMC();
 float convertToRad(float degrees);
-float Calculate_Distance(float lat1, float long1, float lat2, float long2);
+float Calculate_Distance(float lat2, float long2);
